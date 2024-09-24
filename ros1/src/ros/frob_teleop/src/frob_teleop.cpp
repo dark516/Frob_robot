c61d51d861e3f9d0f2331af7771cf55a51fdf4c6
Alex Kulagin <sashakulagin2007@gmail.com>
2024-09-24 06:26:35 +0600

    remove ros1 support ha-ha

--- ros1/src/ros/frob_teleop/src/frob_teleop.cpp
+++
@@ -1,8 +0,0 @@
-#include "frob_teleop.h"
-
-int main(int argc, char** argv) {
-	ros::init(argc, argv, "frob_teleop");
-	ros::NodeHandle private_node("~");
-	FrobTeleop frobTeleop(private_node);
-	ros::spin();
-}
