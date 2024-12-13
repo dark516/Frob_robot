21e20cf78fe1679de9ee8a2e9a63861e0561c33e
alex <sashakulagin2007@gmail.com>
2024-12-13 12:32:33 +0300

    add slam and new descritpion package WARNING: need to test

---
+++ ros2/src/ros2_ws/src/description/setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages, setup
+import os
+from glob import glob
+
+package_name = 'description'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
+        (os.path.join('share', package_name, 'urdf'), glob('urdf/*.urdf')),
+        (os.path.join('share', package_name, 'rviz'), glob('rviz/*.rviz')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='wodo',
+    maintainer_email='moswodocanal@gmail.com',
+    description='robot description',
+    license='no license',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': []
+    },
+)
+
